617a80c10a43be4c53e12701d93c4c7412f93f43
Sharmina Akhtar <sakht318@gmail.com>
2018-05-29 05:25:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
 const driver = {}
 
 function updateDriverWithKeyAndValue(driver, key, value) {
-  
     return Object.assign({}, driver, { [key]: value });
+}
 
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+  driver[key] = value;
+
+ return driver;
 }
