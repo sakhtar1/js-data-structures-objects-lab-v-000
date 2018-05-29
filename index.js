a4cd6011404fdb925e9cd45bac35a1f7e4b31a3e
Sharmina Akhtar <sakht318@gmail.com>
2018-05-29 05:27:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
 }
 
 function deleteFromDriverByKey(driver, key){
-  delete driver.key
+  delete Object.assign(driver.key)
 }
